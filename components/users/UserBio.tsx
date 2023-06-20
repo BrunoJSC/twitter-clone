ba636c2cb24e5faa82f1d4347deafe667f720a93
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-06-19 23:53:05 -0300

    add novos arquivos

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,9 +1,12 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
-import useEditModal from "@/hooks/useEditModal";
-import useUser from "@/hooks/useUser";
 import { format } from "date-fns";
 import { useMemo } from "react";
 import { BiCalendar } from "react-icons/bi";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
+import useFollow from "@/hooks/useFollow";
+import useUser from "@/hooks/useUser";
+
 import Button from "../Button";
 
 interface UserBioProps {
@@ -16,47 +19,60 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 
   const editModal = useEditModal();
 
-  const createdAt = useMemo(() => {
-    if (!fetchedUser?.createdAt) return null;
+  const { isFollowing, toggleFollow } = useFollow(userId);
 
-    return format(new Date(fetchedUser?.createdAt), "MMMM, yyyy");
+  const createdAt = useMemo(() => {
+    if (!fetchedUser?.createdAt) {
+      return null;
+    }
+
+    return format(new Date(fetchedUser.createdAt), "MMMM yyyy");
   }, [fetchedUser?.createdAt]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
-          <Button label="Edit" secondary onClick={editModal.onOpen} />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button label="Follow" secondary onClick={() => {}} />
+          <Button
+            onClick={toggleFollow}
+            label={isFollowing ? "Unfollow" : "Follow"}
+            secondary={!isFollowing}
+            outline={isFollowing}
+          />
         )}
       </div>
-
-      <div className="mt-8 px-4 ">
+      <div className="mt-8 px-4">
         <div className="flex flex-col">
           <p className="text-white text-2xl font-semibold">
-            {currentUser?.name}
+            {fetchedUser?.name}
           </p>
           <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
         </div>
         <div className="flex flex-col mt-4">
-          <p className="text-white">
-            {fetchedUser?.bio ?? "This user has no bio."}
-          </p>
-
-          <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500">
+          <p className="text-white">{fetchedUser?.bio}</p>
+          <div
+            className="
+              flex 
+              flex-row 
+              items-center 
+              gap-2 
+              mt-4 
+              text-neutral-500
+          "
+          >
             <BiCalendar size={24} />
-            <p>Join {createdAt}</p>
+            <p>Joined {createdAt}</p>
           </div>
         </div>
         <div className="flex flex-row items-center mt-4 gap-6">
-          <div className="flex flex-row text-center gap-1">
-            <p className="text-white">{fetchedUser?.followersIds?.length}</p>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followingIds?.length}</p>
             <p className="text-neutral-500">Following</p>
           </div>
-
-          <div className="flex flex-row text-center gap-1">
-            <p className="text-white">{fetchedUser?.followersCount ?? 0}</p>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followersCount || 0}</p>
             <p className="text-neutral-500">Followers</p>
           </div>
         </div>
